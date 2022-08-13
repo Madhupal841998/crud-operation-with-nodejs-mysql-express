59a4a0623988bd056f7c023a814cd17b2d9c6517
mad.zero <33372049+Madhupal841998@users.noreply.github.com>
2022-08-13 13:17:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
 
+const resultsPerPage = 10;
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/register.html');
 });
@@ -36,17 +38,47 @@ app.post('/', (req, res) => {
 
 
 
+
+app.get('/product', (req, res) => {
+    var sql = 'SELECT * FROM product';
+    con.query(sql, (err, result) => {
+            const numOfResults = result.length;
+        const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
+        let page = req.query.page ? Number(req.query.page) : 1;
+        if(page > numberOfPages){
+            res.redirect('/product?page='+encodeURIComponent(numberOfPages));
+        }else if(page < 1){
+            res.redirect('/product?page='+encodeURIComponent('1'));
+        }
+        //Determine the SQL LIMIT starting number
+        const startingLimit = (page - 1) * resultsPerPage;
+        //Get the relevant number of POSTS for this starting page
+        var sql = `SELECT * FROM product LIMIT ${startingLimit},${resultsPerPage}`;
+        con.query(sql, (err, result)=>{
+            let iterator = (page - 5) < 1 ? 1 : page - 5;
+            let endingLink = (iterator + 9) <= numberOfPages ? (iterator + 9) : page + (numberOfPages - page);
+            if(endingLink < (page + 4)){
+                iterator -= (page + 4) - numberOfPages;
+            }
+            res.render(__dirname +'/product', { product: result, page, iterator, endingLink, numberOfPages});
+        });
+    });
+});
+
+/*
 app.get('/product', (req, res) => {
 
     var sql = "select * from product";
 
     con.query(sql, (error, result) => {
-        if (error) throw error;
+        if (error)
+            throw error;
         // console.log(result);
         res.render(__dirname + "/product.ejs", { product: result });
 
     });
 });
+*/
 
 app.get('/delete-product', (req, res) => {
 
@@ -63,9 +95,9 @@ app.get('/delete-product', (req, res) => {
 
 app.get('/update-product', function (req, res) {
     var sql = "select * from product where productId=?";
-
+    
     var productId = req.query.productId;
-
+    
     con.query(sql, [productId], function (error, result) {
         if (error) console.log(error);
         res.render(__dirname + "/update-product", { product: result });
@@ -79,18 +111,12 @@ app.post('/update-product', function (req, res) {
     var categoryName = req.body.categoryName;
     var categoryId = req.body.categoryId;
     
-    
-    
     var sql = " UPDATE product set productId= ?, productName= ? , categoryName= ? , categoryId= ? WHERE id= ? ";
-       
-    con.query(sql,[productId,productName,categoryName,categoryId,id], (result) => {
-            res.redirect('/product');
-        });
+
+    con.query(sql, [productId, productName, categoryName, categoryId, id], (result) => {
+        res.redirect('/product');
+    });
 });
-
-
-
-
 
 /*con.connect(function(error){
     if(error) throw error;
@@ -102,4 +128,4 @@ app.post('/update-product', function (req, res) {
     });
 });
 */
-app.listen(9000);
+app.listen(2000);
